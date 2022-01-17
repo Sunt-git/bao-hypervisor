1cbebb4c305215b647dddfcc741267b29f14c59c
Sandro Pinto <sandro2pinto@gmail.com>
2022-01-17 15:38:27 +0000

    arch: modify armv8 inc struct

---
+++ src/arch/armv8/armv8-r/inc/arch/cache.h
@@ -0,0 +1,24 @@
+/**
+ * Bao, a Lightweight Static Partitioning Hypervisor
+ *
+ * Copyright (c) Bao Project (www.bao-project.org), 2019-
+ *
+ * Authors:
+ *      Sandro Pinto <sandro@bao-project.org>
+ *      Afonso Santos <afomms@gmail.com>
+ *
+ * Bao is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation, with a special exception exempting guest code from such
+ * license. See the COPYING file in the top-level directory for details.
+ *
+ */
+
+#ifndef __ARCH_CACHE_H__
+#define __ARCH_CACHE_H__
+
+#include <bao.h>
+
+#define CACHE_MAX_LVL 0 // TBD
+
+#endif /* __ARCH_CACHE_H__ */
